4ff24edfd080a42245d07db684ce36eb28b0c1c9
PeterW1992 <PeterW1992@users.noreply.github.com>
2017-05-11 20:26:34 +0000

    Changes to file path references from GPSLogger to GPS-Logger

--- Code/StartUp.sh
+++ Code/StartUp.sh
@@ -3,7 +3,7 @@
 # navigate to home directory, then to this directory, then execute python scripts then back home
 sleep 5
 cd /
-cd home/pi/GPSLogger/Code
+cd home/pi/GPS-Logger/Code
 sudo hciconfig hci0 piscan
 sudo python GPSLogger.py &
 sudo python BlueServer.py &
