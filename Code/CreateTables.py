2c222cf59a3f179006332dac12ac2bc74f9a4b75
PeterW1992 <williams1802@hotmail.com>
2017-07-06 22:59:59 +0000

    Empty Database

--- Code/CreateTables.py
+++ Code/CreateTables.py
@@ -22,7 +22,7 @@ stayPointPKFK = " PRIMARY KEY(StartTime,EndTime) FOREIGN KEY (stayPointID) REFER
 
 # tblJourneys columns
 startStayPoint = " StartStayPoint INTEGER"; endStayPoint = " EndStayPoint INTEGER";
-startPointPKFK = " PRIMARY KEY (StartTime) FOREIGN KEY (startStayPoint) REFERENCES %s(ROWID)," % (tblSP); endPointFK = " FOREIGN KEY (endStayPoint) REFERENCES %s(ROWID)" % (tblSP)
+startPointPKFK = " PRIMARY KEY (StartTime) FOREIGN KEY (startStayPoint) REFERENCES %s(ROWID)" % (tblSP); endPointFK = " FOREIGN KEY (endStayPoint) REFERENCES %s(ROWID)" % (tblSP)
 
 # tblUpdate columns
 updateType = "Type TEXT"; updateDT = "DateTime TEXT"; updateStart = "StartTime REAL"; updateEnd = "EndTime REAL"
@@ -34,21 +34,21 @@ journeyPointPKFK = "PRIMARY KEY (pointID) FOREIGN KEY (JourneyID) REFERENCES tbl
 # tblErrors columns
 errorType = " Type TEXT"; fileName = " File TEXT"; sysTime = " SystemTime REAL"; gpsTime = " gpsTime TEXT"; errorDesc = " ErrorDesc TEXT"
 
-createTb11 = "CREATE TABLE %s (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)" % (tblGPSP, lat, lon, dateTime, alt, speed, epx, epy, epz, ept, mode, track)
+createTbl1 = "CREATE TABLE %s (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)" %(tblGPSP, lat, lon, dateTime, alt, speed, epx, epy, epz, ept, mode, track)
 createTbl2 = "CREATE TABLE %s (%s, %s)" % (tblSP, lat, lon)
 createTbl3 = "CREATE TABLE %s (%s, %s, %s, %s)" % (tblSPV, stayPointId, startTime, endTime, stayPointPKFK)
 createTbl4 = "CREATE TABLE %s (%s, %s, %s, %s, %s, %s)" % (tblJs, startStayPoint, endStayPoint, startTime, endTime, startPointPKFK, endPointFK)
-createTbl5 = "CREATE TABLE %s (%s, %s, %s, %s,)" % (tblUpdates, updateType, updateDT, updateStart, updateEnd)
-createTbl6 = "CREATE TABLE &s (%s, %s, %s)" %(tblJourneyPoints, journeyId, pointId, journeyPointPKFK)
-createTbl7 = "CREATE TABLE %s (%s, %s, %s, %s, %s)" %(tblErrors, errorType, fileName, sysTime, gpsTime, errorDesc)
+createTbl5 = "CREATE TABLE %s (%s, %s, %s, %s)" % (tblUpdates, updateType, updateDT, updateStart, updateEnd)
+createTbl6 = "CREATE TABLE %s (%s, %s, %s)" % (tblJourneyPoints, journeyId, pointId, journeyPointPKFK)
+createTbl7 = "CREATE TABLE %s (%s, %s, %s, %s, %s)" % (tblErrors, errorType, fileName, sysTime, gpsTime, errorDesc)
 
-#cur.execute(createTbl1)
-#cur.execute(createTbl2)
-#cur.execute(createTbl3)
-#cur.execute(createTbl4)
-#cur.execute(createTbl5)
-#cur.execute(createTbl6)
-#cur.execute(createTbl7)
+cur.execute(createTbl1)
+cur.execute(createTbl2)
+cur.execute(createTbl3)
+cur.execute(createTbl4)
+cur.execute(createTbl5)
+cur.execute(createTbl6)
+cur.execute(createTbl7)
 
 conn.commit()
 cur.close()
